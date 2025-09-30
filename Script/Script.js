2ca0c80ae94180bccbe10d067bf6d59604137b42
EgorKrinich <krinicnyjegor@gmail.com>
2025-09-30 15:45:27 +0300

    Header + Burger menu

---
+++ Script/Script.js
@@ -0,0 +1,28 @@
+const button = document.querySelectorAll('[data-button]')
+
+button.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const clickTarget = btn.dataset.button
+
+        const menu = document.querySelectorAll(`[data-menu="${clickTarget}"]`)
+        const button = document.querySelector(`[data-button="${clickTarget}"]`)
+
+        menu.forEach(menu => menu.classList.toggle('active'))
+        button.classList.toggle('active')
+    })
+})
+document.addEventListener('click', (e) => {
+  const activeMenus = document.querySelectorAll('[data-menu].active')
+
+  activeMenus.forEach(menu => {
+    const target = menu.dataset.menu
+    const button = document.querySelector(`[data-button="${target}"]`)
+
+    if (!menu.contains(e.target) &&
+        !button.contains(e.target)) 
+        {
+      menu.classList.remove('active')
+      button.classList.remove('active')
+    }
+  })
+})
